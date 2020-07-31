279ca2d13ab42c0f4cf15f56a14d8cd50dddb6b7
Gena <daogena@gmail.com>
2020-07-31 15:31:26 -0500

    Added work section

---
+++ src/Components/Work.js
@@ -0,0 +1,18 @@
+import React from 'react'; 
+import '../style/Work.css'; 
+
+function Work() {
+    return (
+        <div id="work">
+            <h1>Work</h1>
+            <div className="project__container">
+                <div className="project1__image"></div>
+                <div className="project__title">Netflix Clone</div>
+                <div className="button">Visit Website</div>
+                <div className="button">GitHub Link</div>
+            </div>
+        </div>
+    )
+}
+
+export default Work
