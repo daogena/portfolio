be1da2111af8f227a25da34b9948ec940bcca98d
Gena <daogena@gmail.com>
2020-07-30 23:08:42 -0500

    Updated nav bar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import '../style/Header.css'; 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Header() {
+    const [show, handleShow] = useState(false); 
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                handleShow(true); 
+            } else {
+                handleShow(false); 
+            }
+        }); 
+        return () => {
+            window.removeEventListener("scroll"); 
+        }; 
+    }, []); 
 
     return (
         <div id="home">
-            <div className="nav">
-                <div className="blur"></div>
+            <div className={`nav ${show && "nav__black"}`}>
                 <div className="current">
                     <a href="#home" className="smoothscroll">Home</a>
                 </div>
