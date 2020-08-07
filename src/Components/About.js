1c40303ac936eca4510fa78bb2a8a6cf7bb434d9
Gena <daogena@gmail.com>
2020-08-07 15:54:45 -0500

    Made about section responsive

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,44 +1,49 @@
 import React from 'react'; 
 import '../style/About.css'; 
+import '../images/profile-pic.jpg'; 
 
 function About() {
     return (
         <div id="about">
-            <div className="profile-pic"></div>
-            <div className="text">
-                <div className="about__container">
-                    <div className="about__blurb">
-                        <h1 className="title">About Me</h1>
-                        <p className="description">I'm currently a student at University of Houston. I love everything about coding, from solving problems to being stuck on a error for hours/days before realizing you're just missing a semicolon. </p>
+            <div className="columns">
+                <div className="profile-pic">
+                    <img src={require('../images/profile-pic.jpg')} className="me"/>
+                </div> 
+                <div className="text">
+                    <div className="about__container">
+                        <div className="about__blurb">
+                            <h1 className="title">About Me</h1>
+                            <p className="description">I'm currently a student at University of Houston. I love everything about coding, from solving problems to being stuck on a error for hours before realizing you're just missing a semicolon. </p>
+                        </div>
                     </div>
-                </div>
-                <div className="skills__container">
-                    <h1 className="title">Skills</h1>
-                    <div className="skills">
-                        <div className="front-end">
-                            <h3>Front-end</h3>
-                            <ul>
-                                <li>HTML</li>
-                                <li>CSS</li>
-                                <li>JavaScript</li>
-                                <li>ReactJS</li>
-                            </ul>
-                        </div>
-                        <div className="back-end">
-                            <h3>Back-end</h3>
-                            <ul>
-                                <li>NodeJS</li>
-                                <li>Express</li>
-                                <li>Restful Web Services</li>
-                                <li>C++</li>
-                            </ul>
-                        </div>
-                        <div className="tools">
-                            <h3>Tools</h3>
-                            <ul>
-                                <li>Github</li>
-                                <li>Firebase</li>
-                            </ul>
+                    <div className="skills__container">
+                        <h1 className="title">Skills</h1>
+                        <div className="skills">
+                            <div className="front-end">
+                                <h3>Front-end</h3>
+                                <ul>
+                                    <li>HTML</li>
+                                    <li>CSS</li>
+                                    <li>JavaScript</li>
+                                    <li>ReactJS</li>
+                                </ul>
+                            </div>
+                            <div className="back-end">
+                                <h3>Back-end</h3>
+                                <ul>
+                                    <li>NodeJS</li>
+                                    <li>Express</li>
+                                    <li>Restful Web Services</li>
+                                    <li>C++</li>
+                                </ul>
+                            </div>
+                            <div className="tools">
+                                <h3>Tools</h3>
+                                <ul>
+                                    <li>Github</li>
+                                    <li>Firebase</li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
