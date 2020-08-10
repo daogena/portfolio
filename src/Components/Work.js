24212469ac227ecbc33adbdc1704cf759530a7e8
Gena <daogena@gmail.com>
2020-08-10 17:22:55 -0500

    Made work section responsive

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -6,7 +6,9 @@ function Work() {
         <div id="work">
             <h1>Work</h1>
             <div className="project__container">
-                <div className="project1__image"></div>
+                <div className="project1__image">
+                    <img src={require('../images/project1.png')} className="project1" />
+                </div>
                 <div className="project__title">Netflix Clone</div>
                 <div className="button">Visit Website</div>
                 <div className="button">GitHub Link</div>
